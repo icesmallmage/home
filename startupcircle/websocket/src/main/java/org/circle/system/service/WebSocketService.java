6f9c2f00ba91ebecc15e458de4ed593e7f305dff
cdmrs <Liu123456!>
2024-04-15 21:01:26 +0800

    20240415

--- startupcircle/websocket/src/main/java/org/circle/system/service/WebSocketService.java
+++ startupcircle/websocket/src/main/java/org/circle/system/service/WebSocketService.java
@@ -1,14 +1,16 @@
 package org.circle.system.service;
 
+import com.circle.common.core.redis.RedisCache;
+import com.circle.common.utils.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -21,19 +23,32 @@ public class WebSocketService {
 
     private final Logger logger = LoggerFactory.getLogger(WebSocketService.class);
 
+    @Autowired
+    public RedisCache redisCache;
+
     /**
      * 保存连接信息
      */
-    private static final Map<String, Session> CLIENTS = new ConcurrentHashMap<>();
+    private static Map<String, Session> CLIENTS = new ConcurrentHashMap<>();
     private static final Map<String, AtomicInteger> TERMINAL_IDS = new HashMap<>();
+
 
 
     @OnOpen
     public void onOpen(@PathParam("terminalId") String terminalId, Session session) throws Exception {
         logger.info(session.getRequestURI().getPath() + "，打开连接开始：" + session.getId());
 
+        Set<Session> setSession = new HashSet<>();
+
         TERMINAL_IDS.put(terminalId, new AtomicInteger(0));
         CLIENTS.put(terminalId, session);
+
+        redisCache.setCacheObject(session.getId(), terminalId);
+        if(StringUtils.isNull(redisCache.getCacheSet(terminalId))){
+            setSession = redisCache.getCacheSet(terminalId);
+        }
+        setSession.add(session);
+        redisCache.setCacheSet(terminalId, setSession);
 
         logger.info(session.getRequestURI().getPath() + "，打开连接完成：" + session.getId());
 
@@ -43,8 +58,17 @@ public class WebSocketService {
     public void onClose(@PathParam("terminalId") String terminalId, Session session) throws Exception {
         logger.info(session.getRequestURI().getPath() + "，关闭连接开始：" + session.getId());
 
+        Set<Session> setSession = new HashSet<>();
+
         CLIENTS.remove(terminalId);
         TERMINAL_IDS.remove(terminalId);
+
+        redisCache.deleteObject(session.getId());
+        if(StringUtils.isNull(redisCache.getCacheSet(terminalId))){
+            setSession = redisCache.getCacheSet(terminalId);
+        }
+        setSession.remove(session);
+        redisCache.setCacheSet(terminalId, setSession);
 
         logger.info(session.getRequestURI().getPath() + "，关闭连接完成：" + session.getId());
     }
@@ -75,7 +99,17 @@ public class WebSocketService {
 
     public void sendMessage(String message, Session session) {
         try {
-            session.getAsyncRemote().sendText(message);
+            Set<Session> setSession = new HashSet<>();
+            // 从redis获取当前登录人的群聊
+            String terminalId = redisCache.getCacheObject(session.getId());
+            if(StringUtils.isNull(redisCache.getCacheSet(terminalId))){
+                setSession = redisCache.getCacheSet(terminalId);
+            }
+            Iterator<Session> iterator = setSession.iterator();
+            while (iterator.hasNext()){
+                Session allSession =  iterator.next();
+                allSession.getAsyncRemote().sendText(message);
+            }
             logger.info("推送消息成功：" + message);
         } catch (Exception e) {
             logger.error("推送异常：" + e);
